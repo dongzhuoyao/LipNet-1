1b4e84d826536812861d3c3972fe86009c0bfa5e
dongzhuoyao <929109550@qq.com>
2017-06-24 20:04:53 +0800

    update

---
+++ scripts/unzip_and_move.py
@@ -0,0 +1,27 @@
+
+import os
+import subprocess
+
+target_dir = '/bdata/hut/lab/riz_LipNet/data/unzipped'
+
+video_str = 's{*}.mpg_vcd.zip'
+align_str = 's{*}.tar'
+
+
+for k in range(1,35):
+    if k == 21:
+        continue
+
+    video = video_str.replace("{*}", str(k))
+    align = align_str.replace("{*}", str(k))
+
+    cmd = ['unzip', video, '-d', os.path.join(target_dir,'s%d'.format(k))]
+    print('unzip ', video)
+    subprocess.call(cmd)
+
+    #mkdir -p 111 && tar -zxvf s1.tar -C 111
+    #tar_target = os.path.join(target_dir, 's%d_align'.format(k))
+    tar_target = target_dir
+    cmd = ['mkdir', '-p', tar_target,'&&','tar','-zxvf',align,'-C',tar_target ]
+    print('tar  ', align)
+    subprocess.call(cmd)

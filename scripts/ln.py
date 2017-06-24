35cfe9bfc7cbdeb77c4b8a1b1cd4118ae3fca632
dongzhuoyao <929109550@qq.com>
2017-06-24 20:49:29 +0800

    u

--- scripts/ln.py
+++ scripts/ln.py
@@ -3,6 +3,6 @@ for k in range(1,35):
     if k == 21:
         continue
 
-    cmd = ['ln', '-s', '/bdata/hut/lab/riz_LipNet/data/unzipped/s%'.format(k), '/bdata/hut/lab/riz_LipNet/training/unseen_speakers/datasets/train/s%'.format(k)]
+    cmd = ['ln', '-s', '/bdata/hut/lab/riz_LipNet/data/unzipped/s{}'.format(k), '/bdata/hut/lab/riz_LipNet/training/unseen_speakers/datasets/train/s{}'.format(k)]
     print('ln ', cmd)
     subprocess.call(cmd)

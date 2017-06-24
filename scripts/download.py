68c97228a63717b9bc7a73d942c8370df21c21bf
dongzhuoyao <929109550@qq.com>
2017-06-24 16:59:14 +0800

    update

---
+++ scripts/download.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+
+data_url = ''
+target_dir = '/bdata/hut/lab/riz_LipNet/data'
+
+video_str = 'http://spandh.dcs.shef.ac.uk/gridcorpus/s{*}/video/s{*}.mpg_vcd.zip'
+align_str = 'http://spandh.dcs.shef.ac.uk/gridcorpus/s{*}/align/s{*}.tar'
+
+
+for k in range(1,35):
+    if k == 21:
+        continue
+
+    video = video_str.replace("s{*}",k)
+    align = align_str.replace("s{*}",k)
+
+    cmd = ['wget', video,'-P',target_dir]
+    print('Downloading ', video)
+    subprocess.call(cmd)
+
+    cmd = ['wget', align,'-P',target_dir]
+    print('Downloading ', align)
+    subprocess.call(cmd)
